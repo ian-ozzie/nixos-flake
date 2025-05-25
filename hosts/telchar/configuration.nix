9d8ed1d65649e4274cb1699e5678a76dd36ee5e5
Ian Oswald <ian@ozzie.io>
2025-05-26 08:16:44 +1000

    host[telchar]: disable dead drive, tailor hyprland

--- hosts/telchar/configuration.nix
+++ hosts/telchar/configuration.nix
@@ -1,44 +1,51 @@
 {
-  pkgs,
-  ...
-}:
-{
   boot = {
-    kernelPackages = pkgs.linuxKernel.packages.linux_6_13;
+    kernelParams = [ "libata.force=1.00:disable" ]; # Disable dead spinning disk that was part of the fusion drive
+
     loader = {
+      efi.canTouchEfiVariables = true;
+
       systemd-boot = {
         enable = true;
       };
-
-      efi.canTouchEfiVariables = true;
-    };
-  };
-
-  programs.ssh.startAgent = true;
-
-  networking = {
-    hostId = "5eec05b7";
-    hostName = "telchar";
-    dhcpcd.enable = true;
-
-    firewall = {
-      allowedTCPPorts = [ ];
-      allowedUDPPorts = [ ];
     };
   };
 
   home-manager.users.ozzie = {
     wayland.windowManager.hyprland.settings = {
+      exec-once = [
+        "[workspace 1] $terminal"
+        "[workspace 8 silent] $browser"
+      ];
+
       monitor = [
         "eDP-1,2560x1440@60,0x0,1"
         "DP-2,1920x1200@60,-1200x-240,1,transform,3"
         "DP-3,1920x1200@60,2560x-240,1,transform,1"
       ];
 
-      exec-once = [
-        "[workspace 1] $terminal"
-        "[workspace 10 silent] $browser"
+      workspace = [
+        "1,monitor:eDP-1"
+        "2,monitor:eDP-1"
+        "3,monitor:eDP-1"
+        "4,monitor:eDP-1"
+        "5,monitor:eDP-1"
+        "6,monitor:eDP-1"
+        "7,monitor:eDP-1"
+        "8,monitor:eDP-1"
+        "9,monitor:DP-2"
+        "10,monitor:DP-3"
       ];
+    };
+  };
+
+  networking = {
+    dhcpcd.enable = true;
+    hostId = "5eec05b7";
+
+    firewall = {
+      allowedTCPPorts = [ ];
+      allowedUDPPorts = [ ];
     };
   };
 
